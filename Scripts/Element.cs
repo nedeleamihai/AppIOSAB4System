23fb705511d95d65d2f4b0a84880f759db3d43bb
nedeleamihai <nedeleamihai11@gmail.com>
2018-04-27 17:38:25 +0300

    Add files via upload

---
+++ Scripts/Element.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Element : MonoBehaviour {
+
+	public Image image;
+	public Text Name;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void Create(Developer developer){
+		image.sprite = developer.image;
+		Name.text = developer.Name;
+	}
+}
